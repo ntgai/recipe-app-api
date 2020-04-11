4b42dc954b259584677da52f2f82fbb23c79cca7
ntgai <me@ntg.ai>
2020-04-11 21:01:36 +0400

    tests email validity in user creating

--- app/core/models.py
+++ app/core/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
 	def create_user(self, email, password=None, **extra_fields):
 		"""Creates and saves a new custom user"""
+		if not email:
+			raise ValueError("A user must have a valid email address")
 		user = self.model(email=self.normalize_email(email), **extra_fields)
 		user.set_password(password)
 		user.save(using=self._db)
