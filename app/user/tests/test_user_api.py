a08f3ca04f625ccf7045ef9e5465659066706e8f
ntgai <me@ntg.ai>
2020-04-13 01:14:49 +0400

    missing name in test_password_too_short payload

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -46,7 +46,7 @@ class PublicUserAPITests(TestCase):
 
 	def test_password_too_short(self):
 		"""Password must be more than 5 characters"""
-		payload = {'email': 'test@ntg.ai', 'password':'pw'}
+		payload = {'email': 'test@ntg.ai', 'password':'pw', 'name': 'Test'}
 		res = self.client.post(CREATE_USER_URL, payload)
 
 		self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
