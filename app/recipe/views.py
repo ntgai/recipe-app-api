a3b5d080f8e73fecdeaa75ba8209a7422ef2cc88
ntgai <me@ntg.ai>
2020-04-16 13:19:54 +0400

    added recipe filter feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -43,8 +43,24 @@ class RecipeViewSet(viewsets.ModelViewSet):
 	queryset = Recipe.objects.all()
 	serializer_class = serializers.RecipeSerializer
 
+	def _params_to_ints(self, qs):
+		"""Convert a string ids to list of integers"""
+		return [int(str_id) for str_id in qs.split(',')]
+
 	def get_queryset(self):
-		return self.queryset.filter(user = self.request.user)
+		tags = self.request.query_params.get('tags')
+		ingredients = self.request.query_params.get('ingredients')
+		queryset = self.queryset
+
+		if tags:
+			tags_ids = self._params_to_ints(tags)
+			queryset = queryset.filter(tags__id__in=tags_ids)
+
+		if ingredients:
+			ingredients_ids = self._params_to_ints(ingredients)
+			queryset = queryset.filter(ingredients__id__in=ingredients_ids)
+
+		return queryset.filter(user = self.request.user)
 
 	def get_serializer_class(self):
 		"""Return appropriate serializer class"""
